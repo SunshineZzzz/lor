b596443b8d503e8b94ddd8a6310d264e30ccd9ee
sunshinezzz <605855436@qq.com>
2025-03-28 14:28:40 +0800

    视图渲染和支持http方法模块修改上传

--- lib/lor/lib/methods.lua
+++ lib/lor/lib/methods.lua
@@ -1,15 +1,26 @@
+-- Comment: 支持的HTTP方法
+
 -- get and post methods is guaranteed, the others is still in process
 -- but all these methods shoule work at most cases by default
 local supported_http_methods = {
-    get = true, -- work well
-    post = true, -- work well
-    head = true, -- no test
-    options = true, -- no test
-    put = true, -- work well
-    patch = true, -- no test
-    delete = true, -- work well
-    trace = true, -- no test
-    all = true -- todo:
+	-- work well
+	get = true,
+	-- work well
+	post = true,
+	-- no test
+	head = true,
+	-- no test
+	options = true,
+	-- work well
+	put = true,
+	-- no test
+	patch = true,
+	-- work well
+	delete = true,
+	-- no test
+	trace = true,
+	-- todo:
+	all = true
 }
 
 return supported_http_methods
--- lib/lor/lib/view.lua
+++ lib/lor/lib/view.lua
@@ -1,3 +1,5 @@
+-- Comment: 视图渲染
+
 local pairs = pairs
 local type = type
 local setmetatable = setmetatable
@@ -8,47 +10,53 @@ local template_new = template.new
 local View = {}
 
 function View:new(view_config)
-    local instance = {}
-    instance.view_enable = view_config.view_enable
-    if instance.view_enable then
-        if ngx.var.template_root then
-            ngx.var.template_root =  view_config.views
-        else
-            ngx.log(ngx.ERR, "$template_root is not set in nginx.conf")
-        end
-    end
-    instance.view_engine = view_config.view_engine
-    instance.view_ext = view_config.view_ext
-    instance.view_layout = view_config.view_layout
-    instance.views = view_config.views
+	local instance = {}
+	-- 是否开启视图
+	instance.view_enable = view_config.view_enable
+	if instance.view_enable then
+		if ngx.var.template_root then
+			ngx.var.template_root = view_config.views
+		else
+			ngx.log(ngx.ERR, "$template_root is not set in nginx.conf")
+		end
+	end
+	-- 视图引擎字符串
+	instance.view_engine = view_config.view_engine
+	-- 视图文件扩展名
+	instance.view_ext = view_config.view_ext
+	-- 视图布局文件名
+	instance.view_layout = view_config.view_layout
+	-- 视图文件所在的目录
+	instance.views = view_config.views
 
-    setmetatable(instance, {__index = self})
-    return instance
+	setmetatable(instance, {__index = self})
+	return instance
 end
 
 function View:caching()
 end
 
 -- to optimize
+-- 渲染视图模板
 function View:render(view_file, data)
-    if not self.view_enable then
-        ngx.log(ngx.ERR, "view is not enabled. you may need `app:conf('view enable', true)`")
-    else
-        local view_file_name = view_file .. "." .. self.view_ext
-        local layout_file_name = self.view_layout .. "." .. self.view_ext
+	if not self.view_enable then
+		ngx.log(ngx.ERR, "view is not enabled. you may need `app:conf('view enable', true)`")
+	else
+		local view_file_name = view_file .. "." .. self.view_ext
+		local layout_file_name = self.view_layout .. "." .. self.view_ext
 
-        local t = template_new(view_file_name)
-        if self.view_layout ~= "" then
-            t = template_new(view_file_name,layout_file_name)
-        end
-        if data and type(data) == 'table' then
-            for k,v in pairs(data) do
-                t[k] = v
-            end
-        end
+		local t = template_new(view_file_name)
+		if self.view_layout ~= "" then
+			t = template_new(view_file_name, layout_file_name)
+		end
+		if data and type(data) == 'table' then
+			for k,v in pairs(data) do
+				t[k] = v
+			end
+		end
 
-        return tostring(t)
-    end
+		return tostring(t)
+	end
 end
 
 return View
