62d84575f092c7548424ee017eaeae2c4ec9c6e6
sunshinezzz <605855436@qq.com>
2025-03-28 14:24:52 +0800

    请求和回执修改上传

--- lib/lor/lib/request.lua
+++ lib/lor/lib/request.lua
@@ -1,4 +1,6 @@
-local sfind = string.find
+-- Comment: HTTP请求解析封装
+
+local sfind = ngx.re.find
 local pairs = pairs
 local type = type
 local setmetatable = setmetatable
@@ -8,68 +10,69 @@ local Request = {}
 
 -- new request: init args/params/body etc from http request
 function Request:new()
-    local body = {} -- body params
-    local headers = ngx.req.get_headers()
+	local body = {}
+	local headers = ngx.req.get_headers()
 
-    local header = headers['Content-Type']
-    -- the post request have Content-Type header set
-    if header then
-        if sfind(header, "application/x-www-form-urlencoded", 1, true) then
-            ngx.req.read_body()
-            local post_args = ngx.req.get_post_args()
-            if post_args and type(post_args) == "table" then
-                for k,v in pairs(post_args) do
-                    body[k] = v
-                end
-            end
-        elseif sfind(header, "application/json", 1, true) then
-            ngx.req.read_body()
-            local json_str = ngx.req.get_body_data()
-            body = utils.json_decode(json_str)
-        -- form-data request
-        elseif sfind(header, "multipart", 1, true) then
-            -- upload request, should not invoke ngx.req.read_body()
-        -- parsed as raw by default
-        else
-            ngx.req.read_body()
-            body = ngx.req.get_body_data()
-        end
-    -- the post request have no Content-Type header set will be parsed as x-www-form-urlencoded by default
-    else
-        ngx.req.read_body()
-        local post_args = ngx.req.get_post_args()
-        if post_args and type(post_args) == "table" then
-            for k,v in pairs(post_args) do
-                body[k] = v
-            end
-        end
-    end
+	local header = headers['Content-Type']
+	-- the post request have Content-Type header set
+	if header then
+		if sfind(header, "application/x-www-form-urlencoded", "jo") then
+			ngx.req.read_body()
+			local post_args = ngx.req.get_post_args()
+			if post_args and type(post_args) == "table" then
+				for k,v in pairs(post_args) do
+					body[k] = v
+				end
+			end
+		elseif sfind(header, "application/json", "jo") then
+			ngx.req.read_body()
+			local json_str = ngx.req.get_body_data()
+			body = utils.json_decode(json_str)
+		-- form-data request
+		elseif sfind(header, "multipart", "jo") then
+			-- upload request, should not invoke ngx.req.read_body()
+			-- parsed as raw by default
+		else
+			ngx.req.read_body()
+			body = ngx.req.get_body_data()
+		end
+	-- the post request have no Content-Type header set will be parsed as x-www-form-urlencoded by default
+	else
+		ngx.req.read_body()
+		local post_args = ngx.req.get_post_args()
+		if post_args and type(post_args) == "table" then
+			for k,v in pairs(post_args) do
+				body[k] = v
+			end
+		end
+	end
 
-    local instance = {
-        path = ngx.var.uri, -- uri
-        method = ngx.req.get_method(),
-        query = ngx.req.get_uri_args(),
-        params = {},
-        body = body,
-        body_raw = ngx.req.get_body_data(),
-        url = ngx.var.request_uri,
-        origin_uri = ngx.var.request_uri,
-        uri = ngx.var.request_uri,
-        headers = headers, -- request headers
+	local instance = {
+		path = ngx.var.uri,
+		method = ngx.req.get_method(),
+		query = ngx.req.get_uri_args(),
+		params = {},
+		body = body,
+		body_raw = ngx.req.get_body_data(),
+		url = ngx.var.request_uri,
+		origin_uri = ngx.var.request_uri,
+		uri = ngx.var.request_uri,
+		headers = headers,
 
-        req_args = ngx.var.args,
-        found = false -- 404 or not
-    }
-    setmetatable(instance, { __index = self })
-    return instance
+		req_args = ngx.var.args,
+		-- 请求是否被找到
+		found = false
+	}
+	setmetatable(instance, { __index = self })
+	return instance
 end
 
 function Request:is_found()
-    return self.found
+	return self.found
 end
 
 function Request:set_found(found)
-    self.found = found
+	self.found = found
 end
 
 return Request
--- lib/lor/lib/response.lua
+++ lib/lor/lib/response.lua
@@ -1,3 +1,5 @@
+-- Comment: HTTP响应封装
+
 local pairs = pairs
 local type = type
 local setmetatable = setmetatable
@@ -7,134 +9,152 @@ local utils = require("lor.lib.utils.utils")
 
 local Response = {}
 
+-- 创建
 function Response:new()
-    --ngx.status = 200
-    local instance = {
-        http_status = nil,
-        headers = {},
-        locals = {},
-        body = '--default body. you should not see this by default--',
-        view = nil
-    }
+	local instance = {
+		http_status = nil,
+		headers = {},
+		locals = {},
+		body = '--default body. you should not see this by default--',
+		view = nil
+	}
 
-    setmetatable(instance, { __index = self })
-    return instance
+	setmetatable(instance, { __index = self })
+	return instance
 end
 
--- todo: optimize-compile before used
+-- 渲染html网页并且响应内容
 function Response:render(view_file, data)
-    if not self.view then
-        ngx.log(ngx.ERR, "`view` object is nil, maybe you disabled the view engine.")
-        error("`view` object is nil, maybe you disabled the view engine.")
-    else
-        self:set_header('Content-Type', 'text/html; charset=UTF-8')
-        data = data or {}
-        data.locals = self.locals -- inject res.locals
+	if not self.view then
+		ngx.log(ngx.ERR, "`view` object is nil, maybe you disabled the view engine.")
+		error("`view` object is nil, maybe you disabled the view engine.")
+	else
+		self:set_header('Content-Type', 'text/html; charset=UTF-8')
+		data = data or {}
+		-- inject res.locals
+		data.locals = self.locals
 
-        local body = self.view:render(view_file, data)
-        self:_send(body)
-    end
+		local body = self.view:render(view_file, data)
+		self:_send(body)
+	end
 end
 
-
+-- 直接响应html网页内容
 function Response:html(data)
-    self:set_header('Content-Type', 'text/html; charset=UTF-8')
-    self:_send(data)
+	self:set_header('Content-Type', 'text/html; charset=UTF-8')
+	self:_send(data)
 end
 
+-- 直接响应json内容
 function Response:json(data, empty_table_as_object)
-    self:set_header('Content-Type', 'application/json; charset=utf-8')
-    self:_send(utils.json_encode(data, empty_table_as_object))
+	self:set_header('Content-Type', 'application/json; charset=utf-8')
+	self:_send(utils.json_encode(data, empty_table_as_object))
 end
 
+-- 重定向
 function Response:redirect(url, code, query)
-    if url and not code and not query then -- only one param
-        ngx.redirect(url)
-    elseif url and code and not query then -- two param
-        if type(code) == "number" then
-            ngx.redirect(url ,code)
-        elseif type(code) == "table" then
-            query = code
-            local q = {}
-            local is_q_exist = false
-            if query and type(query) == "table" then
-                for i,v in pairs(query) do
-                    tinsert(q, i .. "=" .. v)
-                    is_q_exist = true
-                end
-            end
+	if url and not code and not query then
+		--  It is 302 ("ngx.HTTP_MOVED_TEMPORARILY") by default.
+		ngx.redirect(url)
+	elseif url and code and not query then
+		if type(code) == "number" then
+			ngx.redirect(url, code)
+		elseif type(code) == "table" then
+			query = code
+			local q = {}
+			local is_q_exist = false
+			if query and type(query) == "table" then
+				for i,v in pairs(query) do
+					tinsert(q, i .. "=" .. v)
+					is_q_exist = true
+				end
+			end
 
-            if is_q_exist then
-                url = url .. "?" .. tconcat(q, "&")
-            end
+			if is_q_exist then
+				url = url .. "?" .. tconcat(q, "&")
+			end
 
-            ngx.redirect(url)
-        else
-            ngx.redirect(url)
-        end
-    else -- three param
-        local q = {}
-        local is_q_exist = false
-        if query and type(query) == "table" then
-           for i,v in pairs(query) do
-               tinsert(q, i .. "=" .. v)
-               is_q_exist = true
-           end
-        end
+			ngx.redirect(url)
+		else
+			ngx.redirect(url)
+		end
+	else
+		local q = {}
+		local is_q_exist = false
+		if query and type(query) == "table" then
+		   for i,v in pairs(query) do
+			   tinsert(q, i .. "=" .. v)
+			   is_q_exist = true
+		   end
+		end
 
-        if is_q_exist then
-            url = url .. "?" .. tconcat(q, "&")
-        end
-        ngx.redirect(url ,code)
-    end
+		if is_q_exist then
+			url = url .. "?" .. tconcat(q, "&")
+		end
+		ngx.redirect(url ,code)
+	end
 end
 
+-- rewrite regrex replacement [last/break];
 function Response:location(url, data)
-    if data and type(data) == "table" then
-        ngx.req.set_uri_args(data)
-        ngx.req.set_uri(url, false)
-    else
-        ngx.say(url)
-        ngx.req.set_uri(url, false)
-    end
+	if data and type(data) == "table" then
+		ngx.req.set_uri_args(data)
+		-- rewrite ^ /foo last;
+		-- 等价于
+		-- ngx.req.set_uri("/foo", true)
+		-- 
+		-- rewrite ^ /foo break;
+		-- 等价于
+		-- ngx.req.set_uri("/foo", false)
+		ngx.req.set_uri(url, false)
+	else
+		-- ngx.say(url)
+		ngx.req.set_uri(url, false)
+	end
 end
 
+-- 发送文本响应
 function Response:send(text)
-    self:set_header('Content-Type', 'text/plain; charset=UTF-8')
-    self:_send(text)
+	self:set_header('Content-Type', 'text/plain; charset=UTF-8')
+	self:_send(text)
 end
 
---~=============================================================
-
+-- 私有方法，用于实际发送响应内容
 function Response:_send(content)
-    ngx.status =  self.http_status or 200
-    ngx.say(content)
+	ngx.status = self.http_status or 200
+	ngx.say(content)
 end
 
+-- 获取响应体
 function Response:get_body()
-    return self.body
+	return self.body
 end
 
+-- 获取响应头
 function Response:get_headers()
-    return self.headers
+	return self.headers
 end
 
+-- 获取响应体
 function Response:get_header(key)
-    return self.headers[key]
+	return self.headers[key]
 end
 
+-- 设置响应体
 function Response:set_body(body)
-    if body ~= nil then self.body = body end
+	if body ~= nil then self.body = body end
 end
 
+-- 设置状态码
 function Response:status(status)
-    ngx.status = status
-    self.http_status = status
-    return self
+	ngx.status = status
+	self.http_status = status
+	return self
 end
 
+-- 设置响应头
 function Response:set_header(key, value)
-    ngx.header[key] = value
+	ngx.header[key] = value
 end
 
 return Response
